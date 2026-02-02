6b544fa3e0095543f70452b4c94f628f767d6e31
igorcamposdeborba <igor_campos5@hotmail.com>
2026-02-02 19:57:29 -0300

    Initial commit

---
+++ src/main/java/org/igorborba/java21/UnnamedVariable.java
@@ -0,0 +1,15 @@
+package org.igorborba.java21;
+
+public class UnnamedVariable {
+    public static void main(String[] args) {
+        int arrayInteger[] = {5, 4, 8, 11};
+
+        int quantity = 0;
+
+        for(int _ : arrayInteger){ // todo: UNNAMED VARIABLE _ (isn't used) - avoid @SuppressWarnings
+            quantity++;
+        }
+        System.out.println(quantity);
+
+    }
+}
