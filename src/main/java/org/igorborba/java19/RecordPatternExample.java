73223ccdd1ea31393737bb0cb4ff8004ff552f40
igorcamposdeborba <igor_campos5@hotmail.com>
2026-02-02 22:13:17 -0300

    Java 19 - Destructuring in Record

--- src/main/java/org/igorborba/java19/RecordPatternExample.java
+++ src/main/java/org/igorborba/java19/RecordPatternExample.java
@@ -7,8 +7,13 @@ record Square(double width, double height) {
 public class RecordPatternExample {
     public static void main(String[] args){
         Square square1 = new Square(2, 3);
+        Object square2 = new Square(2, 3);
 
         System.out.println(square1.width());
         System.out.println(square1);
+
+        if (square2 instanceof Square(double w, double h)){
+            System.out.println("Rectangle with Width= " + w + " Height= " + h);  // todo: DESTRUCTURING in Record: appoint to variable and not square.getWith()
+        }
     }
 }
