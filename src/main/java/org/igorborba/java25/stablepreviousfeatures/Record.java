86ea91efbcc73c34c01fd5492782d73d53132032
igorcamposdeborba <igor_campos5@hotmail.com>
2026-02-23 20:24:40 -0300

    Improved theory about Record

--- src/main/java/org/igorborba/java25/stablepreviousfeatures/Record.java
+++ src/main/java/org/igorborba/java25/stablepreviousfeatures/Record.java
@@ -1,12 +1,24 @@
 package org.igorborba.java25.stablepreviousfeatures;
 
 // todo: RECORD: create automatically get, set, constructor, equals, hashcode, toString. It's an immutable class
-record User(int id, String name) {
+record ResultValue(double amount, String currency) {
 }
+/* Record keep data (is a data carrier), not model object (it is not instantiated/object).
+Because of this, it's a value object in DDD. It's immutable, the value matters not the instantiation, keep simple values without complex behavior.
+todo: Use Record to simple objects, immutable (value objects in DDD). Example: record Email(String value), Amount(BigDecimal amount, String currency), TaxId
+
+Restrictions:
+- can't extends, or be abstract, or implements interface, or have instance variables, because it haven't identity (it not build an object, but a data).
+- all attributes are private and final
+- we can implements interface
+- we can have static methods or normal methods
+ */
 
 public class Record {
     public static void main(String[] args) {
-        User user1 = new User(1, "Igor Borba");
-        System.out.println(user1.id()); // get id is created automatically by RECORD
+        ResultValue totalAmount = new ResultValue(1000, "Igor Borba");
+//        totalAmount.setAmount(2000); // haven't setter because it is immutable
+
+        System.out.println(totalAmount.amount()); // get id is created automatically by RECORD
     }
 }
